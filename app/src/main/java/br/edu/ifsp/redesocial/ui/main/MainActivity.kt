7255e08adbe29d450b7d1e1b0dea5c50e59b2b6a
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-03-31 08:58:48 -0300

    Reestruturando projeto e adicionando nova Activity.

--- app/src/main/java/br/edu/ifsp/redesocial/ui/main/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/redesocial/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package br.edu.ifsp.redesocial
+package br.edu.ifsp.redesocial.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.redesocial.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
