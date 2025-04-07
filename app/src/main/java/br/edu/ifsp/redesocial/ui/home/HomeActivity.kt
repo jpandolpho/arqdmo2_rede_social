9d0f036acd30ba1769c72dfaafa345dd59e057cf
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-04-07 11:13:13 -0300

    Adicionando verificação de nulos na hora de entrar no perfil.

--- app/src/main/java/br/edu/ifsp/redesocial/ui/home/HomeActivity.kt
+++ app/src/main/java/br/edu/ifsp/redesocial/ui/home/HomeActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.redesocial.databinding.ActivityHomeBinding
 import br.edu.ifsp.redesocial.ui.main.MainActivity
+import br.edu.ifsp.redesocial.ui.profile.ProfileActivity
+import br.edu.ifsp.redesocial.ui.util.Base64Converter
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -16,13 +20,38 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupScreen()
         setupListener()
+    }
+
+    private fun setupScreen() {
+        val db = Firebase.firestore
+        val email = firebaseAuth.currentUser!!.email.toString()
+        db.collection("usuarios").document(email).get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val document = task.result
+                    if(!document.data.isNullOrEmpty()) {
+                        val imageString = document.data!!["fotoPerfil"].toString()
+                        val bitmap = Base64Converter.stringToBitmap(imageString)
+                        binding.profilePicture.setImageBitmap(bitmap)
+                        binding.username.text = document.data!!["username"].toString()
+                        binding.fullname.text =
+                            document.data!!["nomeCompleto"].toString()
+                    }
+                }
+            }
     }
 
     private fun setupListener() {
         binding.buttonLogout.setOnClickListener {
             firebaseAuth.signOut()
             startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+
+        binding.buttonProfile.setOnClickListener {
+            startActivity(Intent(this,ProfileActivity::class.java))
             finish()
         }
     }
