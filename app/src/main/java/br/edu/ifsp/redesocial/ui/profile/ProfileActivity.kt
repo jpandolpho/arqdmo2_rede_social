102af3f17b983009db09d5ba26bf198b49d227c8
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-04-07 10:51:29 -0300

    Adicionando salvamento de perfil na aplicação.

--- app/src/main/java/br/edu/ifsp/redesocial/ui/profile/ProfileActivity.kt
+++ app/src/main/java/br/edu/ifsp/redesocial/ui/profile/ProfileActivity.kt
@@ -1,5 +1,6 @@
 package br.edu.ifsp.redesocial.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -7,10 +8,16 @@ import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.redesocial.databinding.ActivityProfileBinding
+import br.edu.ifsp.redesocial.ui.home.HomeActivity
+import br.edu.ifsp.redesocial.ui.util.Base64Converter
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
     private lateinit var galeria: ActivityResultLauncher<PickVisualMediaRequest>
+    private val firebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +35,28 @@ class ProfileActivity : AppCompatActivity() {
                     ActivityResultContracts.PickVisualMedia.ImageOnly
                 )
             )
+        }
+
+        binding.buttonSave.setOnClickListener {
+            if (firebaseAuth.currentUser != null){
+                val email = firebaseAuth.currentUser!!.email.toString()
+                val username = binding.textUsername.text.toString()
+                val nomeCompleto = binding.textFullname.text.toString()
+                val fotoPerfilString = Base64Converter.drawableToString(binding.profilePicture.
+                drawable)
+                val db = Firebase.firestore
+                val dados = hashMapOf(
+                    "nomeCompleto" to nomeCompleto,
+                    "username" to username,
+                    "fotoPerfil" to fotoPerfilString
+                )
+                db.collection("usuarios").document(email)
+                    .set(dados)
+                    .addOnSuccessListener {
+                        startActivity(Intent(this, HomeActivity::class.java))
+                        finish()
+                    }
+            }
         }
     }
 
